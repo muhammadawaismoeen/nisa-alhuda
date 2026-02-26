b7b09732c3383e3f6dc29e01e4cd0313fc86929b
muhammadawaismoeen <engineer.awaismoeen@gmail.com>
2026-02-26 05:29:12 +0500

    Finalizing environment variables

--- barakah-app/src/supabaseClient.js
+++ barakah-app/src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Replace these with the keys you just copied from Step 2
-const supabaseUrl = 'https://ozdytjomsqwiprldsevv.supabase.co'; 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96ZHl0am9tc3F3aXBybGRzZXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNDQ0NjYsImV4cCI6MjA4NzYyMDQ2Nn0.j8PGGE_MoHSX7Wd7Zjb4TY24ummDzE54iZwCKt-P4yM'; 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
